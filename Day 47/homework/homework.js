f7d52a1e82615f2db74588c7ed7a7fe9a840b5b8
Lukachiti <chitidze.luka@outlook.com>
2024-10-20 17:23:03 +0400

    Add files via upload

---
+++ Day 47/homework/homework.js
@@ -0,0 +1,27 @@
+function secondifi(nimutes){
+    return mirutes * 60
+
+}
+
+function whatever(min1, min2, min3) {
+    return (min1 + min2) * min3
+}
+
+function or_what(numb1, numb2) {
+    return numb1 > numb2
+}
+
+
+/* I dont get that one */
+
+function last(id, str) {
+document.getElementById(id).textContent = str
+}
+
+last(trex, broo)
+
+/*why isn't is working 
+i have been tring to fix the issue 
+but i cant find where it is*/
+
+
